db277c1e88655eff8c6c2605decc79b9ecb5c9da
Leila Omar <31266644+LeilaOmar@users.noreply.github.com>
2018-03-31 05:08:59 -0700

    Started chrome extension implementation

---
+++ extension/tab_switching.js
@@ -0,0 +1,16 @@
+$('#stats a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+$('#alert a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+$('#notif a').on('click', function (e) {
+    e.preventDefault()
+     $(this).tab('show')
+})
+$('#myTab a[href="#account"]').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
